50d87f47bf4df4e45bc318e0364daaca6d9d4408
Đạt <93239546+C1021G1datnvn@users.noreply.github.com>
2022-08-17 10:40:05 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    titleWeb: "emiu Phương Changggg",
+    introTitle: 'Phương Tranggg XinhĐẹp',
+    introDesc: `Em biết điều gì tuyệt vời nhất thế gian không, là chữ đầu tiên của câu này đấy.',
+    title: 'Với thế giới thì em chỉ là một người. Còn với anh, em là cả thế giới. 🥰',
+    desc: 'Nếu mỗi lần nhớ tới em anh được 500 đồng chắc giờ này anh đã vượt xa Bill Gates.',
+    btnYes: 'Yêu bé bụng mỡ nhiều nhiềuuu<33',
+    btnNo: 'yêu bé bụng mỡ nhiểu:3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Chang lại yêu emm <3',
+    btnReply: 'Gửi cho bé Đạt <3',
+    reply: 'Bắt buộc yêu nhaaaa <33333333',
+    mess: 'Em biết mà 🥰. Yêu bé Chang nhiều nhiều 😘😘',
+    messDesc: 'Xem rồi thì thơm bé Đạt nhiều cái nhaaa !',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://beChangiuuuuuuuuuuuucuaaaaANHHHHHH' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
